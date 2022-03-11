080d73a50137422d8b1a12e0792bff5afbf7c355
Abdulrahman Abdullah <96795264+aabdullah014@users.noreply.github.com>
2022-03-11 12:31:11 -0700

    credential upload
    Fixed issue where no data would load on the page that listed credentials; made it user-specific

--- app.py
+++ app.py
@@ -23,17 +23,24 @@ pass_txt = Entry(root, width = 20)
 user_txt.grid(column = 1, row = 3)
 pass_txt.grid(column = 1, row = 4)
 
+
 # function to display text when button clicked
 def clicked():
     connection = sqlite3.connect('passwordlist.db')
     cursor = connection.cursor()
 
-    query = "SELECT user_id FROM users WHERE username = ? AND password = ?"
+    query = "SELECT username, password FROM users"
     username = user_txt.get()
-    password = user_txt.get()
-    result = cursor.execute(query,(username, password))
-    row = result.fetchone()[0]
-    validateUser(row, username, password)
+    password = pass_txt.get()
+    result = cursor.execute(query)
+    row = result.fetchall()
+    if username and password and (username, password) in row:
+        query = "SELECT user_id FROM users WHERE username = ? AND password = ?"
+        result = cursor.execute(query,(username, password))
+        row = result.fetchone()
+        validateUser(row[0], username, password)
+    else:
+        label.configure(text = "Please try again", fg = "red")
 
 def refresh(username):
     connection = sqlite3.connect('passwordlist.db')
@@ -41,17 +48,20 @@ def refresh(username):
 
     query = "SELECT user_id FROM users WHERE username = ? AND password = ?"
     username = user_txt.get()
-    password = user_txt.get()
-    result = cursor.execute(query,(str(username), str(password)))
-    user_id = result.fetchone()[0]
-    loginSuccess(user_id, username)
+    password = pass_txt.get()
+    if username and password:
+        result = cursor.execute(query,(str(username), str(password)))
+        row = result.fetchone()
+        loginSuccess(row, username)
+    else:
+        label.configure(text = "Please try again", fg = "red")
 
-def create_credentials(website, username, password):
+def create_credentials(website, username, password, user_id):
     root.withdraw()
     connection = sqlite3.connect('passwordlist.db')
     cursor = connection.cursor()
-    query = "INSERT INTO passwords (website, username, password) VALUES (?, ?, ?)"
-    cursor.execute(query, (str(website), str(username), str(password)))
+    query = "INSERT INTO passwords (website, username, password, pass_id) VALUES (?, ?, ?, ?)"
+    cursor.execute(query, (str(website), str(username), str(password), user_id))
     connection.commit()
     connection.close()
 
@@ -64,11 +74,7 @@ def register(username, password):
     username = user_txt.get()
     password = user_txt.get()
     result = cursor.execute(query,(username, password))
-    user_id = result.fetchone()[0]
-
-    query = "SELECT * FROM users"
-    result = cursor.execute(query)
-
+    user_id = result.fetchone()
     loginSuccess(user_id, username)
 
 # create button
@@ -96,12 +102,13 @@ def loginSuccess(user_id, username):
     # btn_update = Button(root, text = "Update", fg = "blue", command = clicked)
     # btn_delete = Button(root, text = "Delete", fg = "blue", command = clicked)
     btn_refresh = Button(root, text = "Refresh", fg = "blue", command = lambda: refresh(username))
-    btn_create = Button(root, text = "Create", fg = "blue", command = lambda: create_screen(username))
+    btn_create = Button(root, text = "Create", fg = "blue", command = lambda: create_screen(user_id))
 
     # btn_update.grid(column = 0, row = 1)
     # btn_delete.grid(column = 1, row = 1)
     btn_refresh.grid(column = 1, row = 1)
     btn_create.grid(column = 2, row = 1)
+
 
     
     query = "SELECT * FROM passwords WHERE pass_id = ?"
@@ -122,7 +129,6 @@ def validateUser(user_id, username, password):
     query = "SELECT username, password FROM users"
     result = cursor.execute(query)
     row = result.fetchall()
-    print(row)
 
     login = (username, password)
     if login in row:
@@ -131,38 +137,44 @@ def validateUser(user_id, username, password):
     else:
         label.configure(text = "Please try again", fg = "red")
 
-def add_entry(website, username, password):
+def add_entry(website, username, password, user_id):
     connection = sqlite3.connect('passwordlist.db')
     cursor = connection.cursor()
 
-    query = "SELECT user_id FROM users WHERE username = ? AND password = ?"
-    username = user_txt.get()
-    password = user_txt.get()
-    result = cursor.execute(query,(username, password))
-    user_id = result.fetchone()[0]
-    create_credentials(website, username, password)
+    if username and password and website:
+        root.quit()
 
-    loginSuccess(user_id, username)
+        create_credentials(website, username, password, user_id)
 
-def create_screen(username):
-    root = Tk()
-    root.title("Password Manager")
-    root.geometry('640x360')
+        loginSuccess(user_id, username)    
+        root.quit()
+
+    else:
+        label.configure(text = "Please try again", fg = "red")
+
+    query = "SELECT * FROM passwords"
+    result = cursor.execute(query)
+    print(result.fetchall())
+
+def create_screen(user_id):
+    root1 = Tk()
+    root1.title("Password Manager")
+    root1.geometry('640x360')
 
     # label to root window e.g. text displayed
-    label = Label(root, text = "What would you like to add?")
+    label = Label(root1, text = "What would you like to add?")
     label.grid()
 
     # addition of entry field
-    web_txt = Entry(root, width = 20)
-    user_txt = Entry(root, width =20)
-    pass_txt = Entry(root, width = 20)
-    web_txt.grid(column = 1, row = 3)
-    user_txt.grid(column = 1, row = 2)
+    web_txt = Entry(root1, width = 20)
+    user_txt = Entry(root1, width =20)
+    pass_txt = Entry(root1, width = 20)
+    web_txt.grid(column = 1, row = 2)
+    user_txt.grid(column = 1, row = 3)
     pass_txt.grid(column = 1, row = 4)
 
 
-    btn = Button(root, text = "Add", fg = "black", command = lambda: add_entry(web_txt.get(), user_txt.get(), pass_txt.get()))
+    btn = Button(root1, text = "Add", fg = "black", command = lambda: add_entry(web_txt.get(), user_txt.get(), pass_txt.get(), user_id))
     btn.grid(column = 1, row = 5)
 
 
